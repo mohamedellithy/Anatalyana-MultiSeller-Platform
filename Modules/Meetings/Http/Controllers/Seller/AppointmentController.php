1b6b6a23292c20fd2b3367d9641296f1206ea1a4
mohamedellithy <develomohamed22@gmail.com>
2023-07-05 13:28:30 +0300

    bbq

--- Modules/Meetings/Http/Controllers/Seller/AppointmentController.php
+++ Modules/Meetings/Http/Controllers/Seller/AppointmentController.php
@@ -24,10 +24,7 @@ class AppointmentController extends Controller
         $response = Http::withHeaders([
             'Authorization' => 'Basic '.base64_encode(env('ZOOM_CLIENT_KEY').":".env('ZOOM_CLIENT_SECRET')),
             'Host'          => 'zoom.us'
-        ])->post("https://zoom.us/oauth/token",[
-            "grant_type" => 'account_credentials',
-            "account_id" => "RhaUP7ffRsim-_1WOB9urQ"
-        ]);
+        ])->post("https://zoom.us/oauth/token?grant_type=account_credentials&account_id=RhaUP7ffRsim-_1WOB9urQ");
 
         dd($response->json());
 
