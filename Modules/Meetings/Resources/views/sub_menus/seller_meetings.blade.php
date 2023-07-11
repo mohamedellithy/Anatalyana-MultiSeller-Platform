8b04adbc1fe75bcf52235bdd07d724594e1ceaf6
mohamedellithy <develomohamed22@gmail.com>
2023-07-11 12:08:56 +0300

    sww

--- Modules/Meetings/Resources/views/sub_menus/seller_meetings.blade.php
+++ Modules/Meetings/Resources/views/sub_menus/seller_meetings.blade.php
@@ -1,13 +1,13 @@
 <div class="top_sub_menu_icons">
     <div class="content-sub-menu d-flex">
         <ul class="">
-            <li class="sub_menu_titles">
+            {{-- <li class="sub_menu_titles">
                 <a class="icon-menu-topbar mb-0 {{ areActiveRoutes(['seller.meetings.appointments.setting_zoom_app']) }}" href="{{  route('seller.meetings.appointments.setting_zoom_app') }}">
                     <label class="fs-14  fw-400 mb-0">
                         {{ translate('Setting Zoom Application') }}
                     </label>
                 </a>
-            </li>
+            </li> --}}
             <li class="sub_menu_titles">
                 <a class="icon-menu-topbar mb-0 {{ areActiveRoutes(['seller.meetings.appointments.index','seller.meetings.appointments.create','seller.meetings.appointments.edit']) }}" href="{{  route('seller.meetings.appointments.index') }}">
                     <label class="fs-14  fw-400 mb-0">
